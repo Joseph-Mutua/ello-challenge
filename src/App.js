6a216ff4b8966ac0ebf3e18233636e1a0430870f
Joseph Mutua <Mutuaj793@gmail.com>
2022-06-01 15:13:03 +0300

    Build the logic to query the API and handle network errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
 import "./App.css";
+
+// Logic to handle GraphQL and Network errors
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+// Build link to query the API
+const link = from([
+  errorLink,
+  new HttpLink({
+    uri: "https://fullstack-engineer-test-n4ouilzfna-uc.a.run.app/graphql",
+  }),
+]);
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <div className="App">
-      <h1>Ello Coding Challenge</h1>
+      <ApolloProvider client={client}>
+        <h3>Ello Coding Challenge</h3>
+      </ApolloProvider>
     </div>
   );
 }
